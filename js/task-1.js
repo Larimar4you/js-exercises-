415a392214bb9879c4a6de2abb08a303d3a08bb9
Larimar4you <Zajanezlaja@gmail.com>
2025-02-03 14:52:49 +0100

    git push

--- js/task-1.js
+++ js/task-1.js
@@ -1,55 +1,13 @@
+`use strict`;
 /* #завдання */
 /*
-Напиши стрілочну функцію getUserNames(users), яка прийматиме один параметр users — 
-масив об’єктів користувачів. Функція має повертати масив імен усіх користувачів 
-(властивість name) із масиву users.
-
+/*
+Завдання 5
+Додайде слухач кліку і визначте, коли клік відбувається
+всередині елемента з id "place" і коли клік припадає поза зоною елемента
+https://developer.mozilla.org/en-US/docs/Web/API/Node/contains
 */
-`use strict`;
-
-const getUserNames = (users) => {  
-  return users.map(user => user.name);
+console.log();
+const  = () => {
+  
 };
-
-/* Перевiрка функцii */
-console.log(
-  getUserNames([
-  {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    balance: 2811
-  },
-  {
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    balance: 3821
-  },
-  {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    balance: 3793
-  },
-  {
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    balance: 2278
-  },
-  {
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    balance: 3951
-  },
-  {
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    balance: 1498
-  },
-  {
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    balance: 2764
-  },
-])
-); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
-
-
