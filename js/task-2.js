bbf4314f71c4755ce296c9425cb0de749a0795e0
Larimar4you <Zajanezlaja@gmail.com>
2025-02-07 15:43:16 +0100

    git push

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,4 @@
 "use strict";
-
-const btn = document.querySelector(".btn-country");
-const countriesContainer = document.querySelector(".countries");
+const request = new XMLHttpRequest();
+request.open("GET", "https://meowfacts.herokuapp.com/");
+console.log(request);
